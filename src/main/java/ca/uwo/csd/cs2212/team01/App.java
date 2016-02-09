95748729032dac789a44f27229724e10dce643bd
Joshua Jackson <jjacks95@uwo.ca>
2016-02-09 11:31:57 -0500

    50% Complete BATut

---
+++ src/main/java/ca/uwo/csd/cs2212/team01/App.java
@@ -0,0 +1,7 @@
+package ca.uwo.csd.cs2212.team01;
+public class App {
+      public static void main ( String [] args ) {
+          System.out.println("Hello Maven World");
+          System.out.println("My username is team01");
+     }
+}
