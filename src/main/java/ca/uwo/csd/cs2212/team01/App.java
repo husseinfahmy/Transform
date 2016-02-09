0d379ea599140d2fa6128faa64d833d75b01898c
Joshua Jackson <jjacks95@uwo.ca>
2016-02-09 11:57:32 -0500

    Team01 BATut Complete

--- src/main/java/ca/uwo/csd/cs2212/team01/App.java
+++ src/main/java/ca/uwo/csd/cs2212/team01/App.java
@@ -1,7 +1,21 @@
 package ca.uwo.csd.cs2212.team01;
-public class App {
-      public static void main ( String [] args ) {
-          System.out.println("Hello Maven World");
-          System.out.println("My username is team01");
-     }
-}
+
+ import org.apache.logging.log4j.LogManager;
+
+ import org.apache.logging.log4j.Logger;
+
+  public class App {
+
+  static Logger logger = LogManager.getLogger (App.class.getName());
+
+  public static void main ( String[] args ) {
+
+    logger.trace("Entering main");
+
+    logger.warn("Hello Maven/log4j World");
+
+    logger.info("My username is team01");
+
+   logger.trace("Exiting main");
+
+} }
