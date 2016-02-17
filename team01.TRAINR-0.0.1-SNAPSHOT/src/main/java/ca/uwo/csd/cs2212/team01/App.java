e69542b08d1edd8cb0860ef1079ca23c92ac7bc0
Kamal El-Bagdady <kelbagda@uwo.ca>
2016-02-17 18:23:15 -0500

    Small changes for git testing.

---
+++ team01.TRAINR-0.0.1-SNAPSHOT/src/main/java/ca/uwo/csd/cs2212/team01/App.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package ca.uwo.csd.cs2212.team01;
+
+// Log4J logging - Josh
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.swing.SwingUtilities;
+
+/**
+ * 
+ */
+public class App {
+	static Logger logger = LogManager.getLogger (App.class.getName());
+	
+    public static void main(String[] args) {
+		logger.trace("Entering main");
+		logger.warn("Hello Maven/log4j World");
+		logger.info("My username is team01");
+		logger.trace("Exiting main");
+		
+		SwingUtilities.invokeLater(new Runnable() {
+        	//@Override
+        	public void run() {
+        		MainWindow window = new MainWindow();
+        		window.setVisible(true);
+        	}
+		});
+	}
+}
