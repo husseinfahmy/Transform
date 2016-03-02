720dd18917a235fcf6272d97e25b18aa191da3d7
Kamal El-Bagdady <kelbagda@uwo.ca>
2016-03-01 21:57:18 -0500

    Test mode read from program arguments as "test"

--- team01.TRAINR-0.0.1-SNAPSHOT/src/main/java/ca/uwo/csd/cs2212/team01/MainWindow.java
+++ team01.TRAINR-0.0.1-SNAPSHOT/src/main/java/ca/uwo/csd/cs2212/team01/MainWindow.java
@@ -36,15 +36,34 @@ public class MainWindow extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 	private int posX = 0, posY = 0;
+	private boolean testMode;
 	
 	static final int MAX_PROGRESS = 1440;
 
+	//Time & Date Stamps:
+	private Date lastCall;
+	
 	private WeighScreen weighScreen;
 	private SplashScreen splashScreen;
 	private DashboardScreen dashboardScreen;
 	
 	public MainWindow(boolean testMode) {
-
+		this.testMode = testMode;
+		
+    	//this.setLayout(null);
+    	//this.setLocation(0, 0);
+    	this.setTitle("TRAINR");
+    	this.setSize(1480, 800);
+    	this.setUndecorated(true);
+    	this.setLocationRelativeTo(null);
+    	this.createMouseListener();
+    	// changed EXIT_ON_CLOSE to DISPOSE_ON_CLOSE, prevents errors when using JFrame
+    	this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    	
+    	if (testMode) {
+    		this.lastCall = new Date();
+    	}
+    	
 		weighScreen = new WeighScreen(this);
 		splashScreen = new SplashScreen(this);
 		dashboardScreen = new DashboardScreen(this);
