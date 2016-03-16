93ac83c818c048de7d31db5d39e4c639b9d8ad51
Nathan Phelps <nphelps3@uwo.ca>
2016-03-16 14:52:12 -0400

    Added daily bests for distance, floors, and steps

--- team01.TRAINR-0.0.1-SNAPSHOT/src/main/java/ca/uwo/csd/cs2212/team01/User.java
+++ team01.TRAINR-0.0.1-SNAPSHOT/src/main/java/ca/uwo/csd/cs2212/team01/User.java
@@ -14,6 +14,10 @@ public class User {
 	public char gender;
 	public float[] height;
 	public LinkedList<Journey> myJourneys = new LinkedList<Journey>();
+	private double bestDistance;
+	private int bestFloors;
+	private int bestSteps;
+	
 	
 	//Constructor
 	/**
@@ -35,5 +39,66 @@ public class User {
 	public int numberOfJourneys()
 	{
 		return myJourneys.size();
+	}
+	
+	/**
+	 * setter method to set the values of the best days for distance, floors, and steps
+	 * @param days a LinkedList of Day objects that store the data for each day
+	 */
+	public void setBestValues(LinkedList<Day> days)
+	{
+		int maxDistance = 0;
+		int maxFloors = 0;
+		int maxSteps = 0;
+		
+		//go through all of the days to find the maximum value for each attribute
+		for(int i = 0; i < days.size(); i++)
+		{
+			if(days.get(i).getTotalDist() > maxDistance)
+			{
+				maxDistance = days.get(i).getTotalDist();
+			}
+			
+			if(days.get(i).getTotalFloors() > maxFloors)
+			{
+				maxFloors = days.get(i).getTotalFloors();
+			}
+			
+			if(days.get(i).getTotalSteps() > maxSteps)
+			{
+				maxSteps = days.get(i).getTotalSteps();
+			}
+		}
+		
+		this.bestDistance = maxDistance;
+		this.bestFloors = maxFloors;
+		this.bestSteps = maxSteps;
+	}
+	
+	/**
+	 * accessor method to get the largest distance in one day
+	 * @return double largest distance in one day
+	 */
+	public double getBestDistance()
+	{
+		return bestDistance;
+	}
+
+	/**
+	 * accessor method to get the largest number of floors in one day
+	 * @return int largest number of floors in one day
+	 */
+	public int getBestFloors()
+	{
+		return bestFloors;
+	}
+
+	/**
+	 * accessor method to get the largest number of steps in one day
+	 * @return int largest number of steps in one day
+	 */
+	public int getBestSteps()
+	{
+		return bestSteps;
 	}
 }
